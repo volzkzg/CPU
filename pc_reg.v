8e975935c3fed7a5c2470d53ab786d96b1e3a46d
Gao Bicheng <volz.kz.g@gmail.com>
2016-08-19 16:57:18 +0800

    Add if_id.v

--- pc_reg.v
+++ pc_reg.v
@@ -1,11 +1,15 @@
+`include "defines.v"
+
 module pc_reg(input wire clk,
-	      input wire rst,
-	      output reg[`InstAddrBus] pc);
+	      input wire 		rst,
+	      output reg [`InstAddrBus] pc
+	      output reg 		ce);
+   
    always @ (posedge clk) begin
       if (rst == `RstEnable) begin
 	 ce <= `ChipDisable;
       end else begin
-	 cs <= `ChipEnable;
+	 ce <= `ChipDisable;
       end
    end
 
